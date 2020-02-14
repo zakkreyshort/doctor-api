dcb065f98fba7206ad1f1372a53267f18c3c53af
Student <example@example.com>
2020-02-14 15:53:49 -0800

    refactor code, create phone number and taking new patients data

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { Doctor } from "./doctor";
 
 
 $(document).ready(function() {
-
+// form for doctor and city/state
     $("#illnessForm").submit(function(event) {
         event.preventDefault();
         const name = $("#inputtedDoctor").val();
@@ -27,14 +27,13 @@ $(document).ready(function() {
             } else if (response.data.length > 0) {
                 for (let i = 0; i <= response.data.length; i++){
                     response.data.forEach(function (doctor) {
-                        $("ul#nameResult").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name} ${doctor.practices[i].visit_address.city} ${doctor.practices[i].visit_address.state} ${doctor.practices[i].visit_address.street} ${doctor.practices[i].visit_address.zip} </li>`);
-
+                        $("ul#nameResult").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name} <br> ${doctor.practices[i].visit_address.city} <br> ${doctor.practices[i].visit_address.state} <br> ${doctor.practices[i].visit_address.street} <br> ${doctor.practices[i].visit_address.zip} <br>  Accepts new patients: ${doctor.practices[i].accepts_new_patients} <br> ${doctor.practices[i].phones[0].number}</li>`);
                      })
                 }
             }
         };
     });
-
+// separate form for symptoms
     $("#symptomForm").submit(function(event) {
         event.preventDefault();
         const symptom = $("#inputtedSymptom").val();
@@ -53,10 +52,10 @@ $(document).ready(function() {
             } else if (response.data.length > 0){
                 for(let i = 0; i <= response.data.length; i++){
                     response.data.forEach(function(doctor){
-                        $("ul#nameResult").append(`<li>${doctor.practices[i].name} <br> ${doctor.practices[i].visit_address.street} <br> ${doctor.practices[i].visit_address.city} <br> ${doctor.practices[i].visit_address.state} <br> ${doctor.practices[i].visit_address.zip} </li>`);
+                        $("ul#nameResult").append(`<li>${doctor.practices[i].name} <br> ${doctor.practices[i].visit_address.street} <br> ${doctor.practices[i].visit_address.city} <br> ${doctor.practices[i].visit_address.state} <br> ${doctor.practices[i].visit_address.zip} <br>  Accepts new patients: ${doctor.practices[i].accepts_new_patients}<br> ${doctor.practices[i].phones[0].number}</li>`);
                     })
                 }
-                    }        
-                }
-            })
+            }        
+        }
+    })
 });
