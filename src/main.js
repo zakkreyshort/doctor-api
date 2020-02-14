5f2b2a7c6a7148d80dd96ff37f183b0478aa725c
Student <example@example.com>
2020-02-14 09:00:16 -0800

    create basic form for user input for illness

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import { Doctor } from "./doctor";
+
+$(document).ready(function(){
+    $().click(function(){
+        
+    })
+})
