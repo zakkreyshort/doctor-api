dfb59603f9c05f064a6f4710a5ef011271efbdef
Student <example@example.com>
2020-02-14 13:32:10 -0800

    create second form for symptoms

--- src/doctor.js
+++ src/doctor.js
@@ -3,9 +3,9 @@ import './../src/main';
 export class Doctor {
 
     
-    async getDoctor(name, symptom, city){
+    async getDoctor(name, city){
         try{
-            let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${symptom}&location=${city}&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+            let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=${city}&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
             if (response.status != 200 && response.ok != 200){
                 return false;
             } else {
@@ -17,4 +17,18 @@ export class Doctor {
         }
     }
     
+    async checkSymptom(symptom){
+        try{
+            let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&user_location=37.773%2C-122.413&skip=0&limit=100&user_key=${process.env.API_KEY}`);
+            if (response.status != 200 && response.ok != 200){
+                return false;
+            } else {
+                let jsonifiedResponse = await response.json();
+                return jsonifiedResponse; 
+            }
+        } catch(error){
+            return false;
+        }
+
+    }
 }
--- src/main.js
+++ src/main.js
@@ -8,44 +8,44 @@ $(document).ready(function(){
 $("#illnessForm").submit(function(event){
         event.preventDefault();
         const name = $("#inputtedDoctor").val();
-        const symptom = $("#inputtedSymptom").val();
         const city = $("#inputtedCity").val();
-    
-        (async () => {
-                let doctor = new Doctor;
-                const response = await doctor.getDoctor(symptom, name, city);
-                console.log(response);
-                getElements(response);
-            })();
-            
-            const getElements = function(response){
-                  if(response === false){
-                      $("#outputHere").text('error in handling request');
-                  } else if (response.data.length>0){
-                      response.data.forEach(function(doctor){
-                        $("ul#nameResult").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name} ${doctor.practices[0].visit_address.city} ${doctor.practices[0].visit_address.state} ${doctor.practices[0].visit_address.street} ${doctor.practices[0].visit_address.zip} </li>`);
-                      })
-                  }          
-     };
-    // $("#doctorForm").submit(function(event){
-    //     event.preventDefault();
         
-    //     (async () => {
-    //         let doctor = new Doctor;
-    //         const response = await doctor.getDoctor(name, city);
-    //         getElements(response);
-    //     })();
+        (async () => {
+            let doctor = new Doctor;
+            const response = await doctor.getDoctor(name, city);
+            getElements(response);
+        })();
+        
+        const getElements = function(response){
+            if(response === false){
+                $("#outputHere").text('error in handling request');
+            } else if (response.data.length>0){
+                response.data.forEach(function(doctor){
+                    $("ul#nameResult").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name} ${doctor.practices[0].visit_address.city} ${doctor.practices[0].visit_address.state} ${doctor.practices[0].visit_address.street} ${doctor.practices[0].visit_address.zip} </li>`);
+                })
+            }          
+        };
+        
+    });
+    
+    $("#symptomForm").submit(function(event){
+        event.preventDefault();
+        const symptom = $("#inputtedSymptom").val();
 
+        (async () => {
+            let doctor = new Doctor;
+            const response = await doctor.checkSymptom(symptom);
+            getElements(response);
+        })();
 
-    //     const getElements = function(response){
-    //         if(response === false){
-    //             $("#otherOutput").text('error in handling request');
-    //         } else if (response.data.length>0){
-    //             response.data.forEach(function(doctor){
-    //                 $("ul#nameResult").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name} ${doctor.practices[0].visit_address.city} ${doctor.practices[0].visit_address.state} ${doctor.practices[0].visit_address.street} ${doctor.practices[0].visit_address.zip} </li>`);
-    //             })
-    //         }
-    //     }
-    // });
+        const getElements = function(response){
+            if(response === false){
+                $("#otherOutputHere").text('error in handling request');
+            } else if (response.data.length>0){
+                response.data.forEach(function(doctor){
+                    $("ul#otherNameResult").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name} ${doctor.practices[0].visit_address.city} ${doctor.practices[0].visit_address.state} ${doctor.practices[0].visit_address.street} ${doctor.practices[0].visit_address.zip} </li>`);
+                })
+            }  
+        };
 })
 })
