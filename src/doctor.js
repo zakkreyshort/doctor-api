19a48949c7219adc478dc142a272ae786c61432b
Student <example@example.com>
2020-02-14 08:53:12 -0800

    add user key and try/catch for fetch data.

---
+++ src/doctor.js
@@ -0,0 +1,17 @@
+export class Doctor {
+    async getIllness(){
+        try{
+            let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}
+            `);
+            let jsonifiedResponse;
+            if (response.ok && response.status == 200){
+                jsonifiedResponse = await response.json();
+            } else {
+                jsonifiedResponse = false;
+            }
+            return jsonifiedResponse;
+        } catch(error) {
+            return false;
+        }
+    }
+}
