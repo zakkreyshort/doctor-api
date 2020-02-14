d8b5c7376965244898810adcb7f775ee62f2c7bf
Student <example@example.com>
2020-02-14 13:19:19 -0800

    work on symptom query

--- src/doctor.js
+++ src/doctor.js
@@ -1,37 +1,20 @@
-import './../src/main'
+import './../src/main';
 
 export class Doctor {
 
     
-    // async getIllness(){
-    //     try{
-    //         let illness = $("#inputtedSymptom").val();
-    //         let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?query=${illness}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}
-    //         `);
-    //         let jsonifiedResponse;
-    //         if (response.ok && response.status == 200){
-    //             jsonifiedResponse = await response.json();
-    //         } else {
-    //             jsonifiedResponse = false;
-    //         }
-    //         return jsonifiedResponse;
-    //     } catch(error) {
-    //         return false;
-    //     }
-    // }
-
-    async getDoctor(name, city){
-    try{
-        let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=${city}&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
-        if (response.status != 200 && response.ok != 200){
+    async getDoctor(name, symptom, city){
+        try{
+            let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${symptom}&location=${city}&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+            if (response.status != 200 && response.ok != 200){
+                return false;
+            } else {
+                let jsonifiedResponse = await response.json();
+                return jsonifiedResponse; 
+            }
+        } catch(error) {
             return false;
-        } else {
-            let jsonifiedResponse = await response.json();
-            return jsonifiedResponse; 
         }
-    } catch(error) {
-        return false;
     }
-}
-
+    
 }
--- src/main.js
+++ src/main.js
@@ -4,49 +4,48 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { Doctor } from "./doctor";
 
-// $("#illnessForm").submit(function(event){
-    //     event.preventDefault();
-    
-    //     (async () => {
-        //         let doctor = new Doctor;
-        //         const response = await doctor.getIllness();
-        //         getElements(response);
-        //     })();
-        //     function getElements(response){
-            //         let doctorArray = [];
-            //         console.log(response); // at this point response is false, why?
-            //         for(let i = 0; i<10; i++){
-                //             doctorArray.push()
-                //         }
-                //         if(response){
-                    //             $("#outputHere").text(response.data);
-                    //         } else{
-                        //             $("#outputHere").text('error in handling request');
-                        //         }
-                        //     }
-                        // });
 $(document).ready(function(){
-    $("#doctorForm").submit(function(event){
+$("#illnessForm").submit(function(event){
         event.preventDefault();
         const name = $("#inputtedDoctor").val();
+        const symptom = $("#inputtedSymptom").val();
         const city = $("#inputtedCity").val();
-        
+    
         (async () => {
-            let doctor = new Doctor;
-            const response = await doctor.getDoctor(name, city);
-            getElements(response);
-        })();
+                let doctor = new Doctor;
+                const response = await doctor.getDoctor(symptom, name, city);
+                console.log(response);
+                getElements(response);
+            })();
+            
+            const getElements = function(response){
+                  if(response === false){
+                      $("#outputHere").text('error in handling request');
+                  } else if (response.data.length>0){
+                      response.data.forEach(function(doctor){
+                        $("ul#nameResult").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name} ${doctor.practices[0].visit_address.city} ${doctor.practices[0].visit_address.state} ${doctor.practices[0].visit_address.street} ${doctor.practices[0].visit_address.zip} </li>`);
+                      })
+                  }          
+     };
+    // $("#doctorForm").submit(function(event){
+    //     event.preventDefault();
+        
+    //     (async () => {
+    //         let doctor = new Doctor;
+    //         const response = await doctor.getDoctor(name, city);
+    //         getElements(response);
+    //     })();
 
 
-        const getElements = function(response){
-            if(response === false){
-                $("#otherOutput").text('error in handling request');
-            } else if (response.data.length>0){
-                response.data.forEach(function(doctor){
-                    console.log(doctor);
-                    $("ul#nameResult").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name} ${doctor.practices[0].visit_address.city} ${doctor.practices[0].visit_address.state} ${doctor.practices[0].visit_address.street} ${doctor.practices[0].visit_address.zip} </li>`);
-                })
-            }
-        }
-    });
+    //     const getElements = function(response){
+    //         if(response === false){
+    //             $("#otherOutput").text('error in handling request');
+    //         } else if (response.data.length>0){
+    //             response.data.forEach(function(doctor){
+    //                 $("ul#nameResult").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name} ${doctor.practices[0].visit_address.city} ${doctor.practices[0].visit_address.state} ${doctor.practices[0].visit_address.street} ${doctor.practices[0].visit_address.zip} </li>`);
+    //             })
+    //         }
+    //     }
+    // });
+})
 })
