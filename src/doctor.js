11c137edabce0d89b69b080ec86d6f49115ea085
Zakkrey Short <zakkreyshort@Zakkreys-MacBook-Pro.local>
2020-02-24 07:48:25 -0800

    change limit in doctorJS to 10.

--- src/doctor.js
+++ src/doctor.js
@@ -18,7 +18,7 @@ export class Doctor {
     
     async checkSymptom(symptom){
         try{
-            let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=100&user_key=${process.env.API_KEY}`);
+            let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
             if (response.status != 200 && response.ok != 200){
                 return false;
             } else {
